3e49b759239774cd0f4329419fc7d9aa4fdc13d1
BHAVANI200602 <ratgrey73@gmail.com>
2026-07-01 16:10:28 +0530

    added components

--- src/components/shaders/ShaderSwitcher.tsx
+++ src/components/shaders/ShaderSwitcher.tsx
@@ -3,8 +3,8 @@ import { lazy, Suspense } from 'react';
 // Lazy-load each shader to keep initial bundle small
 const Shader1LineWaves      = lazy(() => import('./Shader1LineWaves'));
 const Shader2Aurora         = lazy(() => import('./Shader2Aurora'));
-const Shader3Beams          = lazy(() => import('./Shader3Beams'));
-const Shader4ColorBends     = lazy(() => import('./Shader4ColorBends'));
+const Shader3Strands        = lazy(() => import('./Shader3Strands'));
+const Shader4Dither         = lazy(() => import('./Shader4Dither'));
 const Shader5DarkVeil       = lazy(() => import('./Shader5DarkVeil'));
 const Shader6FaultyTerminal = lazy(() => import('./Shader6FaultyTerminal'));
 const Shader7Waves          = lazy(() => import('./Shader7Waves'));
@@ -12,8 +12,8 @@ const Shader7Waves          = lazy(() => import('./Shader7Waves'));
 const SHADERS = [
   Shader1LineWaves,
   Shader2Aurora,
-  Shader3Beams,
-  Shader4ColorBends,
+  Shader3Strands,
+  Shader4Dither,
   Shader5DarkVeil,
   Shader6FaultyTerminal,
   Shader7Waves,
